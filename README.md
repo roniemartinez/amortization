7bcb1bda23011d65a04965f6bcfc7cf565d13008
Ronie Martinez <ronmarti18@gmail.com>
2019-05-05 03:05:58 +0800

    PyPI package

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # amortization
+
 Python library for calculating amortizations and generating amortization schedules
+
+## Demo
+
+[Amortization Calculator](https://apps.easyaspy.org/amortization-calculator)
+
+## Install
+
+```bash
+pip install amortization
+```
+
+## Usage
+
+### Python
+
+#### Amortization Amount
+
+```python
+from amortization import calculate_amortization_amount
+
+amount = calculate_amortization_amount(150_000, 0.1, 36)
+```
+
+### Amortization Schedule
+
+```python
+from amortization import amortization_schedule
+
+for number, amount, interest, principal, balance in amortization_schedule(150_000, 0.1, 36):
+    print()
+```
+
+### Amortization Schedule (using tabulate)
+
+```python
+from amortization import amortization_schedule
+from tabulate import tabulate
+
+table = (x for x in amortization_schedule(150_000, 0.1, 36))
+print(
+    tabulate(
+        table,
+        headers=["Number", "Amount", "Interest", "Principal", "Balance"],
+        floatfmt=",.2f",
+        numalign="right"
+    )
+)
+```
+
+### Command line
+
+```bash
+amortization -h
+usage: amortization [-h] -P PRINCIPAL -n PERIOD -r INTEREST_RATE [-s]
+
+Python library for calculating amortizations and generating amortization
+schedules
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -P PRINCIPAL, --principal PRINCIPAL
+                        Principal amount
+  -n PERIOD, --period PERIOD
+                        Total number of periods
+  -r INTEREST_RATE, --interest-rate INTEREST_RATE
+                        Interest rate per period
+  -s, --schedule        Generate amortization schedule
+```
+
+
+## Dependencies
+
+- [tabulate](https://bitbucket.org/astanin/python-tabulate)
+
+## Author
+
+[Ronie Martinez](ronmarti18@gmail.com)
+
+## References
+
+- [Packaging and distributing projects](https://packaging.python.org/guides/distributing-packages-using-setuptools/)
---
+++ amortization.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# __author__ = "Ronie Martinez"
+# __copyright__ = "Copyright 2019, Ronie Martinez"
+# __credits__ = ["Ronie Martinez"]
+# __maintainer__ = "Ronie Martinez"
+# __email__ = "ronmarti18@gmail.com"
+
+
+def calculate_amortization_amount(principal, interest_rate, period):
+    """
+    Calculates Amortization Amount per period
+
+    :param principal: Principal amount
+    :param interest_rate: Interest rate per period
+    :param period: Total number of periods
+    :return: Amortization amount per period
+    """
+    x = (1 + interest_rate) ** period
+    return principal * (interest_rate * x) / (x - 1)
+
+
+def amortization_schedule(principal, interest_rate, period):
+    """
+    Generates amortization schedule
+
+    :param principal: Principal amount
+    :param interest_rate: Interest rate per period
+    :param period: Total number of periods
+    :return: Rows containing period, interest, principal, balance, etc
+    """
+    amortization_amount = calculate_amortization_amount(principal, interest_rate, period)
+    number = 1
+    balance = principal
+    while number <= period:
+        interest = balance * interest_rate
+        principal = amortization_amount - interest
+        balance = balance - principal
+        yield number, amortization_amount, interest, principal, balance if balance > 0 else 0
+        number += 1
+
+
+def main():
+    import argparse
+    from tabulate import tabulate
+
+    parser = argparse.ArgumentParser(
+        description='Python library for calculating amortizations and generating amortization schedules')
+    parser.add_argument('-P', '--principal', dest='principal', type=float, required=True, help='Principal amount')
+    parser.add_argument('-n', '--period', dest='period', type=int, required=True, help='Total number of periods')
+    parser.add_argument('-r', '--interest-rate', dest='interest_rate', type=float, required=True,
+                        help='Interest rate per period')
+    parser.add_argument('-s', '--schedule', dest='schedule', default=False, action='store_true',
+                        help='Generate amortization schedule')
+    arguments = parser.parse_args()
+    if arguments.schedule:
+        table = (x for x in amortization_schedule(arguments.principal, arguments.interest_rate, arguments.period))
+        print(
+            tabulate(
+                table,
+                headers=["Number", "Amount", "Interest", "Principal", "Balance"],
+                floatfmt=",.2f",
+                numalign="right"
+            )
+        )
+    else:
+        amount = calculate_amortization_amount(arguments.principal, arguments.interest_rate, arguments.period)
+        print("Amortization amount: {:,.2f}".format(amount))
+
+
+if __name__ == '__main__':
+    main()
